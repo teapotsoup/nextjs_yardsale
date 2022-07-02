f21e7c88df24f1d2afaa2c8bd5b85c469ccc0089
teapotsoup <0902ab@naver.com>
2022-07-03 00:56:43 +0900

    prisma setting

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,28 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
+import Link from "next/link";
 
 const Chats: NextPage = () => {
   return (
+    <Layout canGoBack >
     <div className="py-10 divide-y-[1px] ">
       {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm  text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
-          </div>
-        </div>
+
+          <Link href={`/chats/${i}`} key={i}>
+            <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+              <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-sm  text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
+            </a>
+          </Link>
       ))}
     </div>
+    </Layout>
+
   );
 };
 
