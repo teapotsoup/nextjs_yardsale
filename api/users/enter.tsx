33eab24de5ff035f3c61047da0db8676ae6c0b59
teapotsoup <0902ab@naver.com>
2022-07-25 22:49:36 +0900

    axios api

--- api/users/enter.tsx
+++ api/users/enter.tsx
@@ -2,13 +2,15 @@ import client from '@libs/server/client'
 import withHandler from '@libs/server/withHandler'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.body)
+  console.log(req.body.data)
+  console.log(req.body.data.email||req.body.data.phone)
+  //req.body는 조회불가.
+  //해결하려면 headers를 설정해야한다
+  //libs/client/useMutation파일에서 헤더스 설정해야한다
   return res.status(200).end()
 }
 
