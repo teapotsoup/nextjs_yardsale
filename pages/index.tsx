5d0f3bb814cfb88e96314eb0b814e8e1d6899a1a
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2023-10-17 10:00:52 +0900

    home screen index file adding comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,62 +5,65 @@ import Layout from "@components/layout";
 import useSWR, { SWRConfig } from "swr";
 import { Product } from "@prisma/client";
 import Head from "next/head";
-import useUser from "@libs/client/useUser";
 import client from "@libs/server/client";
 
 export interface ProductWithCount extends Product{
-  _count:{favs:number}
+    _count:{favs:number}
 }
 
 type ProductsResponse={
-  ok:boolean;
-  products:ProductWithCount[]
+    ok:boolean;
+    products:ProductWithCount[]
 }
-// Next Js의 모든 페이지는 정적 페이지이다
-// 만든 페이지를 가져가서 일반 html로 만들기 때문이다.
+
 const Home: NextPage = () => {
-const {user} = useUser();
-  const {data} = useSWR<ProductsResponse>("/api/products")
-  return (
-    <Layout hasTabBar title="홈" seoTitle={'Home'}>
-        <Head>
-            <title>Home</title>
-        </Head>
-      <div className="space-y-5 divide-y-[2px]">
-          {data
-              ? data?.products?.map((product) => (
-                  <Item
-                      id={product.id}
-                      key={product.id}
-                      title={product.name}
-                      price={product.price}
-                      hearts={product._count?.favs || 0}
-                  />
-              ))
-              : "Loading..."}
-      </div>
-      <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
-          </svg>
-        </FloatingButton>
-    </Layout>
-  );
+    const {data} = useSWR<ProductsResponse>("/api/products")
+    return (
+        <Layout hasTabBar title="홈" seoTitle={'Home'}>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <div className="space-y-5 divide-y-[2px]">
+                {data
+                    ? data?.products?.map((product) => (
+                        <Item
+                            id={product.id}
+                            key={product.id}
+                            title={product.name}
+                            price={product.price}
+                            hearts={product._count?.favs || 0}
+                        />
+                    ))
+                    : "Loading..."}
+            </div>
+            <FloatingButton href="/products/upload">
+                <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
+                </svg>
+            </FloatingButton>
+        </Layout>
+    );
 };
 
 const Page: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
+    // SWRConfig
+    // "/api/products" swr이 캐시를 불러올때 사용하는 키.
+    // fallback을 통해 캐시 초기값을 설정해줄수 있다.
+
+    // fallback 작성시 기존 백엔드 api에서 데이터를 주는 형태와 똑같이 작성해야한다.
+    // export default 하는건 Home이 아닌 page 컴포넌트이다.
     return (
         <SWRConfig
             value={{
@@ -78,7 +81,6 @@ const Page: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
 };
 
 export async function getServerSideProps(){
-    // console.log("SSR");
     const products = await client?.product.findMany({});
     return {
         props:{
