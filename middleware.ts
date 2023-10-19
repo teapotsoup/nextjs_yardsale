fb886b3a2442b6d1691babb547b7149abb7328fe
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2023-10-19 14:19:36 +0900

    middleware.ts fix #3 (almost same with OG + delete userAgent)

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,14 @@ import {
     NextFetchEvent,
     NextRequest,
     NextResponse,
-    userAgent,
 } from 'next/server';
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-    const ua = userAgent(req);
-    if (ua.isBot) {
-        return new NextResponse("plz don't be a bot. Be human,", {
-            status: 403,
-        });
-    }
+    // const ua = userAgent(req);
+    // if (ua.isBot) {
+    //     return new NextResponse("plz don't be a bot. Be human,", {
+    //         status: 403,
+    //     });
+    // }
     if (!req.url.includes("/api")) {
         if (!req.url.includes("/enter") && !req.cookies.has('carrot-session')) {
             return NextResponse.redirect(new URL("/enter", req.url));
